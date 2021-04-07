be063ff10eaa188a792ad68b498d766e0fc20d3f
gabrieldocs <mydocs.lucas@gmail.com>
2021-04-07 07:48:47 -0300

    inserindo senha no schema para o usuario

--- app/src/schemas/User.ts
+++ app/src/schemas/User.ts
@@ -3,14 +3,16 @@ import { Schema, model, Document } from 'mongoose'
 interface UserInterface extends Document {
     email?:string,
     firstName?:string,
-    lastName?: string
+    lastName?: string,
+    password?: string
 }
 
 
 const UserSchema = new Schema({
     email: String, 
     firstName: String, 
-    lastName: String 
+    lastName: String,
+    password: String 
 },
 {
     timestamps: true
